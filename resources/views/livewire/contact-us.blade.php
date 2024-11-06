016d171ba946c8a8a6cb44bcdfd07351252ed9cf
kangjud <ezuddinshamil14@gmail.com>
2024-11-07 00:04:26 +0800

    edit contact us

--- resources/views/livewire/contact-us.blade.php
+++ resources/views/livewire/contact-us.blade.php
@@ -10,11 +10,11 @@
                 </div>
                 <div class="mb-6">
                     <h3 class="text-xl font-semibold text-emerald-800 mb-2">Contact</h3>
-                    <p class="text-emerald-600">Phone: +60 3-1234 5678</p>
-                    <p class="text-emerald-600">Email: info@masjidbthon.com</p>
+                    <p class="text-emerald-600">Phone: 011-1146 7006</p>
+                    <p class="text-emerald-600">Email: 	mbthoteammultimedia@gmail.com</p>
                 </div>
                 <div class="flex space-x-4">
-                    <a href="https://www.facebook.com/affendy.arifin.3" target="_blank" class="text-emerald-600 hover:text-emerald-700 transition duration-300">
+                    <a href="https://www.facebook.com/p/Masjid-Bandar-Tun-Hussein-Onn-100064325539673/" target="_blank" class="text-emerald-600 hover:text-emerald-700 transition duration-300">
                         <span class="sr-only">Facebook</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/css/filament/admin/theme.css",
-                "resources/views/**/*.blade.php",
+
             ],
             refresh: true,
         }),
