75b2b9999bb67bc6b93c89d7aec752b89f8f7028
Black Kaiser <khairin13chan@gmail.com>
2025-01-14 21:35:35 +0800

    user can only view registered club in dashboard

--- app/Filament/Resources/ClubResource/Pages/ListClubs.php
+++ app/Filament/Resources/ClubResource/Pages/ListClubs.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\ClubResource\Pages;
 
 use App\Filament\Resources\ClubResource;
+use App\Models\Club;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListClubs extends ListRecords
 {
@@ -15,5 +17,20 @@ class ListClubs extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        $user = auth()->user();
+
+        // Admins can view all clubs
+        if ($user->isAdmin()) {
+            return Club::query();
+        }
+
+        // Regular users can only view clubs they have joined
+        return Club::query()->whereHas('users', function ($query) use ($user) {
+            $query->where('user_id', $user->id);
+        });
     }
 }
